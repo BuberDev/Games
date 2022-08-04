1d141221f3d470c0de57c1d40a0e4b116600b7dc
Dawid Bubernak <92649325+BuberDev@users.noreply.github.com>
2022-08-04 09:38:17 +0200

    Create index.js

---
+++ Tic Tac Toe/index.js
@@ -0,0 +1,78 @@
+const fieldsElement = document.querySelectorAll(".board__item");
+let fields = ["", "", "", "", "", "", "", "", ""];
+const panel = document.querySelector(".panel");
+const button = document.querySelector("button");
+
+let activePlayer = "X";
+panel.innerHTML = ` ${activePlayer} spielt jetzt`;
+let gameActive = true;
+
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+];
+const displayWinMesssage = () => {
+  panel.innerHTML = `Glückwunsch  ${
+    activePlayer === "X" ? (activePlayer = "O") : (activePlayer = "X")
+  }, du hast gewonnen!`;
+  panel.classList.add("active");
+};
+const displayTieMessage = () => {
+  panel.innerHTML = `Zeichnen! Spiel es nochmal`;
+  panel.classList.add("active");
+};
+const changePlayer = () => {
+  activePlayer = activePlayer === "X" ? "O" : "X";
+  panel.innerHTML = ` ${activePlayer} spielt jetzt`;
+};
+const validateGame = () => {
+  let gameWon = false;
+  for (let i = 0; i < 8; i++) {
+    const [posA, posB, posC] = winningConditions[i];
+    const value1 = fields[posA];
+    const value2 = fields[posB];
+    const value3 = fields[posC];
+    if (value1 !== "" && value1 === value2 && value2 === value3) {
+      gameWon = true;
+      break;
+    }
+  }
+  if (gameWon) {
+    gameActive = false;
+    displayWinMesssage();
+  } else if (isBoardFull()) {
+    gameActive = false;
+    displayTieMessage();
+  }
+};
+
+const isBoardFull = () => {
+  return fields.find((field) => field === "") === undefined;
+};
+
+fieldsElement.forEach((field) => {
+  field.addEventListener("click", (e) => {
+    const { pos } = e.target.dataset;
+    if (gameActive && fields[pos] === "") {
+      fields[pos] = activePlayer;
+      e.target.classList.add(`board__item--filled--${activePlayer}`);
+      changePlayer();
+      validateGame();
+    }
+  });
+
+  button.addEventListener("click", () => {
+    field.classList.remove("board__item--filled--X", "board__item--filled--O");
+    fields = ["", "", "", "", "", "", "", "", ""];
+    activePlayer = "X";
+    panel.innerHTML = ` ${activePlayer} spielt jetzt`;
+    panel.classList.remove("active");
+    gameActive = true;
+  });
+});
